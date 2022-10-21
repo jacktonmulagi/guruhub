7e86d5983b932d6bcb04df1c4da3a0f6bdcc6428
jacktonmulagi <otingajacton@gmail.com>
2022-10-21 16:20:22 +0300

    font styles

--- pages/index.js
+++ pages/index.js
@@ -15,19 +15,19 @@ export default function Home() {
         <meta charset="utf-8" />
         <title>Pazoori Coffee Kenya -We specialize in sourcing the highest quality coffee </title>
         <meta name="description"
-          content="  At Pazoori we specialize in sourcing the highest quality coffee,<br /> coffee exports, coffee imports and are soon launching our roastery" />
+          content="  By virtue of being licensed coffee dealers, we source and supply a wide variety of coffee beans that are tested for quality by our in-house coffee cupper" />
         <meta property="og:url " content="https://www.pazoori.com/" />
-        <meta property="og:title" content=" By virtue of being licensed coffee dealers, we source<br /> and supply a wide variety of coffee beans that are tested for quality by our in-house coffee cupper" />
+        <meta property="og:title" content=" At Pazoori we specialize in sourcing the highest quality coffee, coffee exports, coffee imports and are soon launching our roastery" />
         <meta property="og:site_name" content="Pazoori Coffee" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://www.pazoori.com/ogBanner.png" />
         <meta property="og:locale" content="en_KE" />
         <meta property="og:description"
-          content="At Pazoori we specialize in sourcing the highest quality coffee,<br /> coffee exports, coffee imports and are soon launching our roastery" />
+          content="At Pazoori we specialize in sourcing the highest quality coffee, coffee exports, coffee imports and are soon launching our roastery" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         <meta name="description"
-          content=" Kenya is renown for some of the worlds finest gourmet coffees and this is what we consistently <br /> grow and produce in our plantations located in the Rift Valley" />
+          content=" Kenya is renown for some of the worlds finest gourmet coffees and this is what we consistently  grow and produce in our plantations located in the Rift Valley" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
