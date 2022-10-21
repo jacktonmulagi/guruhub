8086b3d625794ee828b80edbb7b459b95b68ffd1
jacktonmulagi <otingajacton@gmail.com>
2022-10-21 15:03:10 +0300

    optimazation

--- pages/index.js
+++ pages/index.js
@@ -8,21 +8,26 @@ import NavBar from '../components/navbar/NavBar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
- 
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Pazoori</title>
-        <meta name="description" content="best coffee near me">
-        </meta>
-        <meta property="og:title" content="Get your favorite coffee from us in minutes">
-        </meta>
-        <meta property="og:description" content="At Pazoori we value our clients ">
-        </meta>
-        <meta property="og:url" content="https://www.pazoori.com/">
-
-        </meta><meta property="og:type" content="website">
-        </meta>
+        <meta charset="utf-8" />
+        <title>Pazoori Coffee Kenya -We specialize in sourcing the highest quality coffee </title>
+        <meta name="description"
+          content="  At Pazoori we specialize in sourcing the highest quality coffee,<br /> coffee exports, coffee imports and are soon launching our roastery" />
+        <meta property="og:url " content="https://www.pazoori.com/" />
+        <meta property="og:title" content=" By virtue of being licensed coffee dealers, we source<br /> and supply a wide variety of coffee beans that are tested for quality by our in-house coffee cupper" />
+        <meta property="og:site_name" content="Pazoori Coffee" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="%PUBLIC_URL%/ogBanner.png" />
+        <meta property="og:locale" content="en_KE" />
+        <meta property="og:description"
+          content="At Pazoori we specialize in sourcing the highest quality coffee,<br /> coffee exports, coffee imports and are soon launching our roastery" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description"
+          content=" Kenya is renown for some of the worlds finest gourmet coffees and this is what we consistently <br /> grow and produce in our plantations located in the Rift Valley" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
